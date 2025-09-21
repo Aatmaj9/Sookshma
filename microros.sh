72cdf70bf917ec1e56abcfffe0fd9eaf2561c554
Aatmaj9 <na22b018@smail.iitm.ac.in>
2025-09-21 17:56:23 +0530

    micro-ros-solved

--- microros.sh
+++ microros.sh
@@ -1,11 +1,7 @@
-#docker run -it --rm -v /dev:/dev --privileged --net=host microros/micro-ros-agent:humble serial --dev /dev/ttyACM0 -v6
-
-# on the Pi host
-docker run --rm -it \
+docker run -d --privileged --rm \
   --name microros_agent \
   --net=host \
-  --privileged \
-  -e ROS_DOMAIN_ID=42 \
-  --device=/dev/serial/by-id/usb-Arduino__www.arduino.cc__Arduino_Due_Prog._Port_XXXXXXXX:/dev/ttyACM0 \
+  --volume="/dev":"/dev" \
+  --device=/dev/ttyACM0 \
   microros/micro-ros-agent:humble \
-  serial --dev /dev/ttyACM0 -b 115200 -v6
+  serial --dev /dev/ttyACM0 -b 115200
