001c6b37aef9c23cdf0725ec6f1da5abe40d6713
Aatmaj9 <na22b018@smail.iitm.ac.in>
2025-09-21 13:40:48 +0530

    testing microros

--- microros.sh
+++ microros.sh
@@ -1,7 +1,1 @@
-docker run -d --privileged --rm \
-  --name microros_agent \
-  --net=host \
-  --env="ROS_DOMAIN_ID=42" \
-  --device=/dev/ttyACM0 \
-  microros/micro-ros-agent:humble \
-  serial --dev /dev/ttyACM0 -b 115200
+docker run -it --rm -v /dev:/dev --privileged --net=host microros/micro-ros-agent:humble serial --dev /dev/ttyACM0 -v6
